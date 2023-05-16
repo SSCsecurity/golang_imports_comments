cf517e95fce34f1bf63142f6ad1c4176a52de6a2
jamesfgreene <jamesfgreene@gmail.com>
2023-05-16 11:18:07 -0700

    Create test.go

---
+++ test.go
@@ -0,0 +1,35 @@
+import {
+  "fmt"
+  "net/http"
+  //"comment"
+  "flag"
+}
+
+/* import {
+  "testing"
+  "errors"
+  "strconv"  
+}
+*/
+
+/*
+import {
+  "io"
+  "regexp"
+  "time"
+}
+*/
+
+
+import { /*
+  "nonsense"
+  "foobar"  
+}
+*/
+  
+import { 
+/*
+  "yessense"
+  "barfoo"  
+}
+*/  
